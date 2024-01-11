07fcf58374567ebf7bc743378faeb558b4a22372
nyxtryx <77376224+nyxtryx@users.noreply.github.com>
2024-01-11 07:07:37 +0000

    Update cpCustMods.py
    slight changes, the details weren't correct - you don't need to pass to obj to call the function (I think... I've not actually used these scripts yet other than for testing individually...)

--- customModules/cpCustMods.py
+++ customModules/cpCustMods.py
@@ -1,19 +1,19 @@
-''' 
- to call, place this file in the root, and run
-    import cpCustMods
-    <object> = cpCustMods.<function>
-
-    for example:
-        import cpCustMods
-        PWD = cpCustMods.CPCWD(<your relative path here>)
-'''
+# place this file in the root of your project!
 
 import os
 import shutil as SH
 herePWD = os.getcwd() + "\\customModules"
 print("Your current root directory is: " +  herePWD + "\n \n Please remember to use relative paths for calling modules! \n \n ie if you have subfolders, you'll need to provide the path FROM the root dir to the sub-dir ie \\<your dir here> to be inserted for any def that has relativePath as a variable.")
 
-# below copies the getCWD file to the PWD+relative path
+''' below copies the getCWD file to the PWD+relative path
+ to call, run
+    import cpCustMods
+    cpCustMods.<function>()
+
+    for example the below will copy the getCWD.py file from your root (ie here) to wherever you specify with obj relativePath being the path to your dir:
+        import cpCustMods
+        cpCustMods.CPCWD(<your relative path here>)
+'''
 def CPCWD(relativePath):
     SH.copy2(herePWD + "\\getCWD.py", (herePWD + relativePath + "\\getCWD.py"), follow_symlinks=True)
     print("Copied getCWD.py from source to: " + herePWD + relativePath + "\\getCWD.py \n \n You may now import getCWD into the current project and call using getCWD()")
